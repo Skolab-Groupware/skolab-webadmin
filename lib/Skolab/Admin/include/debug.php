6ae9e6c8559f749a385d7802fad12353fb52f5f8
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2017-11-17 22:24:35 +0100

    lib/Skolab/Admin/include/debug.php: Rework indentation.

--- lib/Skolab/Admin/include/debug.php
+++ lib/Skolab/Admin/include/debug.php
@@ -21,50 +21,54 @@
  */
 
 function getmicrotime() {
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);
+	list($usec, $sec) = explode(" ", microtime());
+	return ((float)$usec + (float)$sec);
 }
 function debug($str) {
-    #print $str.'<br/>';
+	// print $str.'<br/>';
 }
 function debug_var_dump($var) {
-    #print '<pre>';
-    #var_dump($var);
-	#print '</pre>';
+	// print '<pre>';
+	// var_dump($var);
+	// print '</pre>';
 }
 function backtrace() {
 	$debug_array = debug_backtrace();
 	$counter = count($debug_array);
 	for($tmp_counter = 0; $tmp_counter != $counter; ++$tmp_counter) {
-	?>
+
+?>
+
  <table width="558" height="116" border="1" cellpadding="0" cellspacing="0" bordercolor="#000000">
     <tr>
     <td height="38" bgcolor="#D6D7FC"><font color="#000000">function <font color="#FF3300"><?
-    echo($debug_array[$tmp_counter]["function"]);?>(</font> <font color="#2020F0">
-    <?php
-    //count how many args a there
-    $args_counter = count($debug_array[$tmp_counter]["args"]);
-    //print them
-    for($tmp_args_counter = 0; $tmp_args_counter != $args_counter; ++$tmp_args_counter) {
-		echo($debug_array[$tmp_counter]["args"][$tmp_args_counter]);
+    echo($debug_array[$tmp_counter]["function"]);?>(</font> <font color="#2020F0"><?php
 
-		if(($tmp_args_counter + 1) != $args_counter) {
-			echo(", ");
-		} else {
-			echo(" ");
+		//count how many args a there
+		$args_counter = count($debug_array[$tmp_counter]["args"]);
+		//print them
+		for($tmp_args_counter = 0; $tmp_args_counter != $args_counter; ++$tmp_args_counter) {
+			echo($debug_array[$tmp_counter]["args"][$tmp_args_counter]);
+
+			if(($tmp_args_counter + 1) != $args_counter) {
+				echo(", ");
+			} else {
+				echo(" ");
+			}
 		}
-    }
+
     ?></font><font color="#FF3300">)</font></font></td></tr><tr>
     <td bgcolor="#5F72FA"><font color="#FFFFFF">{</font><br>
       <font color="#FFFFFF">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;file:
        <?php echo($debug_array[$tmp_counter]["file"]);?></font><br>
         <font color="#FFFFFF">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;line:
        <?php echo($debug_array[$tmp_counter]["line"]);?></font><br>
-	<font color="#FFFFFF">}</font></td></tr></table>
+        <font color="#FFFFFF">}</font></td></tr></table>
        <?php
-	   if(($tmp_counter + 1) != $counter) {
-		   echo("<br>was called by:<br>");
-	   }
+
+		if(($tmp_counter + 1) != $counter) {
+			echo("<br>was called by:<br>");
+		}
 	}
 	//exit();
 }
